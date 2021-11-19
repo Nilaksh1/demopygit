ba355d7c5f1ddfce275e669456309cb128b93b60
Nilaksh Vasudeva <nilaksh.vasudeva@gmail.com>
2021-11-19 22:52:40 +0530

    Even Number Code completed

--- evencode.py
+++ evencode.py
@@ -1,1 +1,12 @@
-Create code for taking input from user and print the if number is even or not 
+
+'''
+Create
+code
+for taking input from user and print the if number is even or not
+'''
+
+n = int(input("Enter the number"))
+if n%2==0:
+    print("It is even number")
+else:
+    print("It is not an even number")
